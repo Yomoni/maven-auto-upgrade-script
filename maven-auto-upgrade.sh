6cf9a5c64a9ded51f9eaf50c9dbdb37246be5bf9
Sylvain Bugat <cidian@laposte.net>
2015-12-22 23:51:44 +0100

    Move function to an external shell lib

--- maven-auto-upgrade.sh
+++ maven-auto-upgrade.sh
@@ -9,125 +9,17 @@
 #Disable git command prompt (since git 2.3)
 export export GIT_TERMINAL_PROMPT=0
 
-function usage
-{
-	sed -e '/^#UsageStart/,/^#UsageEnd/!d' -e 's/^#//' -e '/^UsageStart/d' -e '/^UsageEnd/d' "${0}" >&2
-}
-
-#Compare 2 versions and return the most important digit found (1:major, 2:minor, 3:patch, -1:unknown)
-function compareVersions
-{
-	declare IFS='.'
-	declare -a version1=( $1 )
-	declare -a version2=( $3 )
-
-	for (( n=0; n<4; n+=1 ))
-	do
-		if [[ "${version1[n]}" =~ '^[0-9]+$' && "${version2[n]}" =~ '^[0-9]+$' ]]
-		then
-			if [[ "${version1[n]}" -gt "${version2[n]}" ]]
-			then
-				return "${n}"
-			elif [[ "${version1[n]}" -lt "${version2[n]}" ]]
-			then
-				return "${n}"
-			fi
-		else
-			if [[ "${version1[n]}" > "${version2[n]}" ]]
-			then
-				return "${n}"
-			elif [[ "${version1[n]}" < "${version2[n]}" ]]
-			then
-				return "${n}"
-			fi
-		fi
-	done
-
-	return -1
-}
-
-#Function du print the commit message on the standard output
-function commitMessage
-{
-	declare upgradedProperty="${1}"
-	declare upgradeOutput="${2}"
-
-	declare versionUpgrade=$( echo "${upgradeOutput}" | grep '^\[INFO\] Updated ${'"${upgradedProperty}"'}' | grep -o "[^ ]* to [^ ]*$" | sed 's/to/->/' )
-
-	#Version comparaison for Emoji arrow type
-	compareVersions ${versionUpgrade}
-	declare versionDelta="${?}"
-	if [[ "${versionDelta}" -eq 0 ]]
-	then
-		declare arrowEmoji=":arrow_double_up:"
-	elif [[ "${versionDelta}" -eq 2 ]]
-	then
-		declare arrowEmoji=":arrow_up_small:"
-	#Default Emoji
-	else
-		declare arrowEmoji=":arrow_up:"
-	fi
-
-	#print the git commit message on the standard output
-	echo ":construction_worker:${arrowEmoji} ${upgradedProperty} upgrade ${versionUpgrade}"
-}
-
-#Function to print the commit details message with updated dependencies on the standard output
-function commitDetails
-{
-	declare upgradedProperty="${1}"
-	declare upgradeOutput="${2}"
-
-	declare versionUpgrade=$( echo "${upgradeOutput}" | grep '^\[INFO\] Updated ${'"${upgradedProperty}"'}' | grep -o "[^ ]* to [^ ]*$" | sed 's/to/->/' )
-
-	declare commitDescription="Upgraded artifact details:\n"
-	declare upgradedArtifactDetails=$( echo "${upgradeOutput}" | grep "^\[INFO\] artifact " | grep -o "[^ ]*:[^ ]*:[^ ]*" | sort -u )
-	while read line
-	do
-		commitDescription="${commitDescription}${line} ${versionUpgrade}\n"
-	done <<< "${upgradedArtifactDetails}"
-
-	#print the git commit details on the standard output
-	echo -e "${commitDescription}"
-}
+typeset -r scriptDir=$( dirname "${0}")
+source "${scriptDir}/lib/git-lib.sh"
 
 #Main
 
-#Git command check
-echo -n "Verifying Git version:..."
-gitVersion=$( git --version 2>&1 )
+#Environment check
+environmentCheck
 if [[ "${?}" -ne 0 ]]
 then
-	echo -e "[\033[31mNOT FOUND\033[0m]"
-	echo "$0 needs Git, check your installation and the PATH environment variable" >&2
 	exit 1
 fi
-echo -e "[\033[32mOK\033[0m] -> ${gitVersion}"
-
-#GitHub hub command check (optionnal)
-echo -n "Verifying GitHub Hub version:..."
-hubVersion=$( hub --version 2>&1 )
-if [[ "${?}" -ne 0 ]]
-then
-	gitCommand="git"
-	echo -e "[\033[33mNOT FOUND\033[0m]"
-	echo "$0 optionally needs GitHub hub command to create some pull-request (https://hub.github.com), check your installation and the PATH environment variable" >&2
-else
-	gitCommand="hub"
-	echo -e "[\033[32mOK\033[0m] -> "$( echo "${hubVersion}" | tail -n 1 )
-fi
-
-#Maven command check
-echo -n "Verifying Maven version:..."
-mavenVersion=$( mvn --version 2>&1 )
-if [[ "${?}" -ne 0 ]]
-then
-	echo -e "[\033[31mNOT FOUND\033[0m]"
-	echo "${mavenVersion}" >&2
-	echo "$0 needs Maven, check your installation and the PATH environment variable" >&2
-	exit 1
-fi
-echo -e "[\033[32mOK\033[0m] -> "$( echo "${mavenVersion}" | awk '{ print $1,$2,$3 ; exit }' )
 
 #Arguments checking
 if [[ "${#}" -lt 1 || "${#}" -gt 2 ]]
